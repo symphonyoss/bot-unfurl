c4459a2b59cfb0468e987f650204860882c8b21c
Peter Monks <pmonks@gmail.com>
2016-11-14 15:31:47 -0800

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -34,8 +34,8 @@
                       [com.github.symphonyoss/clj-symphony "e2b970a975" :exclusions [org.clojure/clojure]]  ; Via jitpack for now
 
                       ; The following dependencies are inherited but have conflicting versions, so we "pin" the versions here
-                      [com.fasterxml.jackson.core/jackson-databind              "2.8.4"]
-                      [com.fasterxml.jackson.core/jackson-annotations           "2.8.4"]
+                      [com.fasterxml.jackson.core/jackson-databind              "2.8.5"]
+                      [com.fasterxml.jackson.core/jackson-annotations           "2.8.5"]
                       [com.fasterxml.jackson.dataformat/jackson-dataformat-yaml "2.8.4"]
                       [joda-time/joda-time                                      "2.9.6"]
                     ]
