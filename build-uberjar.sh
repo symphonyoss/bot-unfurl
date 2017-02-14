c19e11c82095270327fcd78b3ebcb6b81aea3df3
Maurizio Pillitu <maoo@symphony.foundation>
2017-02-14 16:47:02 +0100

    fixed syntax error on build-uberjar.sh

--- build-uberjar.sh
+++ build-uberjar.sh
@@ -2,4 +2,4 @@
 
 mkdir -p target/uberjar
 
-mv \"$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')\" target/uberjar/bot-unfurl-standalone.jar
+mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" target/uberjar/bot-unfurl-standalone.jar
