09947a4e1308b73d3beb679e5a62ab1503a4ab03
Maurizio Pillitu <maoo@symphony.foundation>
2017-02-14 18:36:06 +0100

    add config.edn for openshift in the uploaded folder

--- build-uberjar.sh
+++ build-uberjar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir -p ./uberjar
+mkdir -p ./uberjar/etc
+
+cp etc/config-openshift.edn ./uberjar/etc/config.edn
 
 mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" uberjar/bot-unfurl-standalone.jar
