07bde00108f3d8542cef7da8510990fa9c2f6212
Maurizio Pillitu <maoo@apache.org>
2017-07-25 17:15:09 +0200

    :ambulance: using resources/config.edn for openshift

--- build-uberjar.sh
+++ build-uberjar.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ./uberjar/etc
 
-cp etc/config-openshift.edn ./uberjar/etc/config.edn
+cp resources/config.edn ./uberjar/etc/config.edn
 cp etc/log4j-openshift.properties ./uberjar/etc/log4j.properties
 
 mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" uberjar/bot-unfurl-standalone.jar
