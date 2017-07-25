e2c272cdc9b2af4ae8a3516c5288636a87633d93
Maurizio Pillitu <maoo@apache.org>
2017-07-25 18:22:35 +0200

    :ambulance: deploying only uberjar to openshift, instead of the uberjar folder

--- build-uberjar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./uberjar/etc
-
-cp resources/config.edn ./uberjar/etc/config.edn
-cp etc/log4j.xml.sample ./uberjar/etc/log4j.xml
-
-mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" uberjar/bot-unfurl-standalone.jar
