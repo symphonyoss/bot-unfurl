4d7814a55fc067833f5023be5d08056b154ce46d
Maurizio Pillitu <maoo@apache.org>
2017-07-25 17:30:13 +0200

    :ambulance: using log4j.xml.sample for openshift deployment

--- build-uberjar.sh
+++ build-uberjar.sh
@@ -3,6 +3,6 @@
 mkdir -p ./uberjar/etc
 
 cp resources/config.edn ./uberjar/etc/config.edn
-cp etc/log4j-openshift.properties ./uberjar/etc/log4j.properties
+cp etc/log4j.xml.sample ./uberjar/etc/log4j.xml
 
 mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" uberjar/bot-unfurl-standalone.jar
