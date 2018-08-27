13a72cffb3be84c710f3e95fca7fc3c02bd84aa8
Peter Monks <pmonks@gmail.com>
2018-08-27 13:06:37 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -32,9 +32,9 @@
                     ]
   :dependencies     [
                       [org.clojure/clojure              "1.9.0"]
-                      [org.apache.commons/commons-lang3 "3.7"]
+                      [org.apache.commons/commons-lang3 "3.8"]
                       [aero                             "1.1.3"]
-                      [mount                            "0.1.12"]
+                      [mount                            "0.1.13"]
                       [org.clojure/tools.cli            "0.3.7"]
                       [org.clojure/tools.logging        "0.4.1"]
                       [ch.qos.logback/logback-classic   "1.2.3"]
@@ -67,7 +67,7 @@
                       [joda-time/joda-time                                          "2.10"]
                       [org.hamcrest/hamcrest-core                                   "1.3"]
                     ]
-  :profiles         {:dev {:dependencies [[midje         "1.9.1"]]
+  :profiles         {:dev {:dependencies [[midje         "1.9.2"]]
                            :plugins      [[lein-midje    "3.2.1"]
                                           [lein-licenses "0.2.2"]]}
                      :uberjar {:aot          :all
